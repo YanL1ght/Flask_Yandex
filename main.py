fe72ecbc5f9078de6b64a96b314f00de6ef27488
YanLight <yan190408@gmail.com>
2024-04-01 18:54:43 +0300

    commit4

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key_for_web_prodject'
 
 
-@app.route('/')
+@app.route('/home')
 def home():
     return render_template('/html_files/main.html')
+
+
+@app.route('/search')
+def search():
+    search_words = request.args.get('q')
+    print(search_words)
+    return render_template('/html_files/search.html')
+
+
+@app.route('/query-example')
+def query_example():
+    # if key doesn't exist, returns None
+    language = request.args.get('language')
+
+    return '''<h1>The language value is: {}</h1>'''.format(language)
 
 
 if __name__ == '__main__':
