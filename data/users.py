30f795d783c67bc2c99d817365c62e3c58efbf5f
YanLight <yan190408@gmail.com>
2024-04-07 18:59:18 +0300

    Добавлена авторизация

---
+++ data/users.py
@@ -0,0 +1,29 @@
+import sqlalchemy
+from data import db_session
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class User(db_session.SqlAlchemyBase, UserMixin):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String,
+                              index=True, unique=True, nullable=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        print()
+        print()
+        print(self.hashed_password)
+        print(password)
+        return check_password_hash(self.hashed_password, password)
+
+    def __repr__(self):
+        return '<User {}, email {}, password {}, xz {}>'.format(
+            self.name, self.email, generate_password_hash(self.hashed_password), check_password_hash(self.hashed_password, self.hashed_password))
--- main.py
+++ main.py
@@ -1,29 +1,76 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, redirect
+from flask_login import LoginManager, login_user, login_required, logout_user
+from data.db_session import *
+from data.users import User
+from data.login_form import LoginForm
+from werkzeug.security import check_password_hash
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key_for_web_prodject'
 
+login_manager = LoginManager()
+login_manager.init_app(app)
 
-@app.route('/home')
+
+@app.route('/', methods=["GET", 'POST'])
 def home():
-    return render_template('/html_files/main.html')
+    if request.method == 'GET':
+        return render_template('/html_files/main.html')
+    elif request.method == 'POST':
+        print(request.form['a_catalog'])
 
 
-@app.route('/search')
+@app.route('/catalog', methods=["GET", 'POST'])
 def search():
-    search_words = request.args.get('q')
+    search_words = request.args
     print(search_words)
-    return render_template('/html_files/search.html')
+    lst = [f'{i} p' for i in range(10)]
+    return render_template('/html_files/catalog.html', items=lst)
 
 
-@app.route('/query-example')
-def query_example():
-    # if key doesn't exist, returns None
-    language = request.args.get('language')
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = create_session()
+    return db_sess.query(User).get(user_id)
 
-    return '''<h1>The language value is: {}</h1>'''.format(language)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = create_session()
+        print(db_sess.query(User).filter(User.email == form.email.data).first())
+        print(form.password.data)
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        print(check_password_hash(user.hashed_password, form.password.data))
+        print()
+        print(user.hashed_password)
+        print(form.password.data)
+        if user and check_password_hash(user.hashed_password, form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('html_files/login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('html_files/login.html', title='Авторизация', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
 
 
 if __name__ == '__main__':
+    global_init('db/users.sqlite')
+    db_ses = create_session()
+    userr = User()
+    userr.name = 'User2'
+    userr.email = 'emaill@email.com'
+    userr.set_password('33333333')
+    db_ses.add(userr)
+    db_ses.commit()
+
     app.run(port=8080, host='127.0.0.1')
